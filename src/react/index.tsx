21352255a36e6959aba81d2b0ec6806209bcf3f3
Max Meinders <max@havelaer.nl>
2025-09-14 11:45:20 +0200

    chore: formatting and pr check

--- src/react/index.tsx
+++ src/react/index.tsx
@@ -1,4 +1,4 @@
-import { type ReactNode, createContext, useContext } from "react";
+import { createContext, type ReactNode, useContext } from "react";
 import type { ArgValue, Formatter } from "..";
 import { partsToJSX } from "./partsToJSX";
 
